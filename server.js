792647c421a346ccfb2028b9ee219b4d9369c9ff
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-07-06 08:51:41 -0700

    installing mongoose

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express()
 //for heroku port
 const PORT = process.env.PORT || 3000;
 
+//for mongoose
+const mongoose = require('mongoose')
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+
+
+//TEST ROUTE
 app.get('/', (req, res)=>{
     res.send('this works...')
 })
 
+//PORT
 app.listen(PORT, ()=>{
   console.log('listening....');
+})
+
+//connect to mongoose
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', ()=>{
+  console.log('connected to mongoose!!!!!!!!!!!');
 })
