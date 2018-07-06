7c4266693af29ff3c9c94f4f3bda05744e5969ba
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-07-06 08:27:24 -0700

    port for keroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+//for heroku port
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
     res.send('this works...')
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
   console.log('listening....');
 })
