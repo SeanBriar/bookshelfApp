edae7e8e950c42985a81d60bb6dd25896525d28f
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-07-11 13:36:00 -0700

    deleted seed route.

--- controllers/books.js
+++ controllers/books.js
@@ -83,56 +83,5 @@ router.get('/books/:id/edit', (req, res)=>{
   })
 })
 
-// seed route
-router.get('/seed/newbooks', (req, res) => {
-  const newBooks =
-  [
-    {
-      title: 'To Kill a Mockingbird',
-      author: 'Harper Lee',
-      img: 'http://hhsmediaweb.weebly.com/uploads/1/3/0/3/13036594/9899555.jpg',
-      synopsis: 'Scout Finch is growing up under extraordinary circumstances in a 1930s sleepy Alabama town of Maycomb.'
-    },
-    {
-      title: 'In the Forest of the Night',
-      author: 'Amelia Atwater-Rhodes',
-      img: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/In_the_Forests_of_the_Night_cover.jpg/220px-In_the_Forests_of_the_Night_cover.jpg',
-      synopsis: 'Tells the story of a three-hundred-year-old vampire named Risika and her struggles throughout her life, both before and after she was transformed. '
-    },
-    {
-      title: 'Dawn',
-      author: 'Octavia E. Butler',
-      img: 'https://sites.tufts.edu/femscifibyanna/files/2015/11/img091.jpg',
-      synopsis: 'Hundreds of years after atomic fire killed her family, Lilith Iyapo awakes, deep in the hold of a massive alien spacecraft piloted by the Oankali—who arrived just in time to save humanity from extinction.'
-    },
-    {
-      title: 'Oryx and Crake',
-      author: 'Maraget Atwood',
-      img: 'https://files.ebook.bike/cover/12705061660836.png',
-      synopsis: 'Post-apocalyptic "Snowman" attempts to survive the distruction of the earth near a group of primitive human-like creatures. Flashbacks reveal that Snowman was once a boy named Jimmy who grew up in a world dominated by multinational corporations and privileged compounds for the families of their employees.'
-    },
-    {
-      title: 'His Dark Materials Series',
-      author: 'Phillip Pullman',
-      img: 'https://images-na.ssl-images-amazon.com/images/I/51dum6TvBYL._SX343_BO1,204,203,200_.jpg',
-      synopsis: 'Two ordinary children on a perilous journey through shimmering haunted otherworlds.'
-    },
-    {
-      title: 'We Should All Be Feminists',
-      author: 'Chimamanda Ngozi Adichie',
-      img: 'https://www.greenwomanstore.com/media/Books/PCW/BookCovers/Non-Fiction/We-should-all-be-feminists-chimamanda-ngozi-adichie3.jpg',
-      synopsis: 'With humor and levity, here Adichie offers readers a unique definition of feminism for the twenty-first century—one rooted in inclusion and awareness.'
-    }
-  ]
-
-
-  Books.create(newBooks, (err, book) => {
-    if (err) { console.log(err) }
-    console.log('seeded')
-    res.redirect('/books')
-  })
-})
-
-
 
 module.exports = router
